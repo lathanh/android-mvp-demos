2402b1725762c4a98e6ea4b367f2fa1c9da354d1
Robert LaThanh <robert@lathanh.com>
2016-03-04 09:47:22 -0800

    Make menu options onClick DRY by enumifying options

--- Demo_app/src/main/java/org/lathanh/demo/android/mvp/MainActivity.java
+++ Demo_app/src/main/java/org/lathanh/demo/android/mvp/MainActivity.java
@@ -7,12 +7,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import org.lathanh.demo.android.mvp.adapting_demo.*;
+import org.lathanh.demo.android.mvp.adapting_demo.AdaptingDemo_Improvement1Fragment;
+import org.lathanh.demo.android.mvp.adapting_demo.AdaptingDemo_Improvement2Fragment;
+import org.lathanh.demo.android.mvp.adapting_demo.AdaptingDemo_StandardFragment;
 import org.lathanh.demo.android.mvp.adapting_demo.data_binding.DataBindingDemo_Improvement1Fragment;
 import org.lathanh.demo.android.mvp.adapting_demo.data_binding.DataBindingDemo_StandardFragment;
 
-
 public class MainActivity extends AppCompatActivity {
+
+  /**
+   * The {@link #onCreateOptionsMenu(Menu) Options Menu} options and where they
+   * should go.
+   */
+  public enum MenuOption {
+    ADAPTING_DEMO_STANDARD(R.id.adaptingDemo_standard) {
+      @Override
+      public Fragment newInstance() {
+        return AdaptingDemo_StandardFragment.newInstance();
+      }
+    },
+    ADAPTING_DEMO_IMPROVEMENT1(R.id.adaptingDemo_improvement1) {
+      @Override
+      public Fragment newInstance() {
+        return AdaptingDemo_Improvement1Fragment.newInstance();
+      }
+    },
+    ADAPTING_DEMO_IMPROVEMENT2(R.id.adaptingDemo_improvement2) {
+      @Override
+      public Fragment newInstance() {
+        return AdaptingDemo_Improvement2Fragment.newInstance();
+      }
+    },
+    DATA_BINDING_DEMO_STANDARD(R.id.dataBindingDemo_standard) {
+      @Override
+      public Fragment newInstance() {
+        return DataBindingDemo_StandardFragment.newInstance();
+      }
+    },
+    DATA_BINDING_DEMO_IMPROVEMENT1(R.id.dataBindingDemo_improvement1) {
+      @Override
+      public Fragment newInstance() {
+        return DataBindingDemo_Improvement1Fragment.newInstance();
+      }
+    },
+    ;
+
+    final int menuId;
+
+    MenuOption(int menuId) {
+      this.menuId = menuId;
+    }
+
+    public abstract Fragment newInstance();
+  }
+
+
+  //-- 'Activity' methods -----------------------------------------------------
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -34,50 +84,30 @@ public class MainActivity extends AppCompatActivity {
   public boolean onOptionsItemSelected(MenuItem item) {
     int id = item.getItemId();
     FragmentManager fragmentManager = getSupportFragmentManager();
-    switch (id) {
-      case R.id.clear:
-        Fragment activeFargment =
-            fragmentManager.findFragmentById(R.id.container);
-        if (activeFargment != null) {
+
+    if (id == R.id.clear) {
+      Fragment activeFragment =
+          fragmentManager.findFragmentById(R.id.container);
+      if (activeFragment != null) {
+        fragmentManager.beginTransaction()
+            .remove(activeFragment)
+            .setBreadCrumbTitle(R.string.clear)
+            .commit();
+      }
+      return true;
+    } else {
+      for (MenuOption menuOption : MenuOption.values()) {
+        if (id == menuOption.menuId) {
           fragmentManager.beginTransaction()
-              .remove(activeFargment)
-              .setBreadCrumbTitle(R.string.clear)
+              .replace(R.id.container, menuOption.newInstance())
+              .setBreadCrumbTitle(item.getTitle())
               .commit();
+          return true;
         }
-        return true;
-      case R.id.adaptingDemo_standard:
-        fragmentManager.beginTransaction()
-            .replace(R.id.container, AdaptingDemo_StandardFragment.newInstance())
-            .setBreadCrumbTitle(R.string.adaptingDemo_standardFragment_name)
-            .commit();
-        return true;
-      case R.id.adaptingDemo_improvement1:
-        fragmentManager.beginTransaction()
-            .replace(R.id.container, AdaptingDemo_Improvement1Fragment.newInstance())
-            .setBreadCrumbTitle(R.string.adaptingDemo_improvement1Fragment_name)
-            .commit();
-        return true;
-      case R.id.adaptingDemo_improvement2:
-        fragmentManager.beginTransaction()
-            .replace(R.id.container, AdaptingDemo_Improvement2Fragment.newInstance())
-            .setBreadCrumbTitle(R.string.adaptingDemo_improvement2Fragment_name)
-            .commit();
-        return true;
-      case R.id.dataBindingDemo_standard:
-        fragmentManager.beginTransaction()
-            .replace(R.id.container, DataBindingDemo_StandardFragment.newInstance())
-            .setBreadCrumbTitle(R.string.dataBindingDemo_standardFragment_name)
-            .commit();
-        return true;
-      case R.id.dataBindingDemo_improvement1:
-        fragmentManager.beginTransaction()
-            .replace(R.id.container, DataBindingDemo_Improvement1Fragment.newInstance())
-            .setBreadCrumbTitle(R.string.dataBindingDemo_improvement1Fragment_name)
-            .commit();
-        return true;
+      }
     }
 
     return super.onOptionsItemSelected(item);
-  }
+  } // onOptionsItemSelected()
 
 }
